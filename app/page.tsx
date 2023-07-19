e6bc5c560e46cfac3d3d193c0a12662b715a4d7a
김용철 <skekdnl12@gmail.com>
2023-07-19 18:01:51 +0900

    broker추가

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,8 @@ export default async function Home() {
 
     const getData = async ():Promise<{curRooms:string[];members:any[]}> => {
         "use server"
-        const fetchData = await fetch("http://localhost:8083/member/members",{method:'GET'})
-        const memberString = await fetchData.json()
-        const members = ['a']
+        const fetchData = await fetch("http://localhost:8083/member/members",{method:'GET', cache: 'no-store'})
+        const members = await fetchData.json()
         const curRooms:string[] = await new Promise<string[]>((resolve, reject) => setTimeout(()=>resolve(['a','b','c']),1000))
         return {members,curRooms}
     }
